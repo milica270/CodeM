0f8efd20bd605d87ce04f1c72eb8024c33cd81cb
milica <vasiljevicmilica@gmail.com>
2024-04-18 19:18:56 +0200

    finalversion2

--- resources/views/shared/left-nav.blade.php
+++ resources/views/shared/left-nav.blade.php
@@ -36,9 +36,10 @@
 
 <div class="card mt-3 mb-3 p-3">
     <h4 style="text-align: center"><span class="bg-success">M</span>{{__('codem.user')}} <i style="color: orange;" class="fa-solid fa-trophy"></i></h4>
-    
+    @if($userWithMostLikes!=null)
     <div class="d-flex align-items-center mt-2">
-        <img src="{{$userWithMostLikes->getImageURL()}}" class="img-fluid rounded-circle" style="width: 25%" alt="">
-        <strong class="ms-1"><a href="{{route('users.show',$userWithMostLikes->id )}}" style="color: rgb(52, 58, 64)">{{$userWithMostLikes->name }}</a></strong>
+        <img src="{{$userWithMostLikes != null ? $userWithMostLikes->getImageURL() : '' }}" class="img-fluid rounded-circle" style="width: 25%" alt="">
+        <strong class="ms-1"><a href="{{route('users.show', $userWithMostLikes != null ? $userWithMostLikes->id : '' )}}" style="color: rgb(52, 58, 64)">{{$userWithMostLikes != null ? $userWithMostLikes->name : '' }}</a></strong>
     </div>
+    @endif
 </div>
